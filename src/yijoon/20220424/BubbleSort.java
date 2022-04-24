183ef4de4b29d7b2b0a648a94d960d420a4751a4
yjkim <yjkim@sweetk.co.kr>
2022-04-24 20:14:57 +0900

    20220424 커밋

---
+++ src/yijoon/20220424/BubbleSort.java
@@ -0,0 +1,18 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] numbers = {8,3,7,5,4,2};
+        for(int i=1; i<numbers.length; i++){            //(총 배열 크기-1 만큼 돌기)
+            for(int j=0; j<numbers.length-1; j++){      //각 비교는 점점 줄어들기
+                if(numbers[j] > numbers[j+1]){
+                    int tmp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = tmp;
+                }
+            }
+            for(int num : numbers){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
